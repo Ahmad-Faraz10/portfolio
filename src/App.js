00d4c392b3344e74c7f1559c90bab3fffda71f68
Ahmad Faraz <2020ahmadfaraz@gmail.com>
2025-05-09 02:23:16 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { StarIcon } from "@chakra-ui/icons";
 import Card from "./Card";
 import Experience from "./Experience";
 import Footer from "./Footer";
@@ -7,6 +8,10 @@ import Skills from "./Skills";
 function App() {
   return (
     <div className="back">
+      <a href="mailto:farazahmad8748@gmail.com" className="star_icon">
+        Hire me
+      </a>
+
       <div className=" container hback">
         <Header />
         <Card />
